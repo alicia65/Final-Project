649a0792c9fd82e273c5170f3e6b386a49c53ac4
alicia65 <alicia65>
2019-12-16 19:06:35 -0600

    Final project is partially done.   I did add more codes to the search button and Doll file.

---
+++ src/main/java/com/alicia/Doll.java
@@ -0,0 +1,14 @@
+package com.alicia;
+
+public class Doll {// creating new class called Doll
+    String name;//create variable to store string value
+    String type;
+
+    Doll(String name, String type) {// constructor contains two parameters: name and type
+         this.name = name;//create object this to pass variable name
+         this.type = type;
+    }
+
+
+
+}
